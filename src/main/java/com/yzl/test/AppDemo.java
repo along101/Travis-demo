0d2ed7abbe7121a6593d10e9c66a7431513a6b2e
yinzuolong <yinzuolong@ppdai.com>
2018-03-13 16:11:33 +0800

    增加测试

---
+++ src/main/java/com/yzl/test/AppDemo.java
@@ -0,0 +1,16 @@
+package com.yzl.test;
+
+public class AppDemo {
+
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+
+    public void testDemo() {
+        System.out.println(456);
+    }
+
+    public void print() {
+        System.out.println(123);
+    }
+}
---
+++ src/test/java/com/yzl/test/AppDemoTest.java
@@ -0,0 +1,12 @@
+package com.yzl.test;
+
+import org.junit.Test;
+
+public class AppDemoTest {
+
+    @Test
+    public void name() {
+        AppDemo demo = new AppDemo();
+        demo.testDemo();
+    }
+}
